a16912a205b06b27600036497dd18a686b531ba0
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-06-29 11:09:23 -0500

    one scatMat needed all combinations of levels for background

--- oneScatSO.R
+++ oneScatSO.R
@@ -8,7 +8,12 @@ library(dplyr)
 library(data.table)
 library(ggplot2)
 library(tibble)
-myPairs <- c("A", "B", "C", "D")
+
+# Create data and subsets of data based on user selection of pairs
+dat <- data.frame(ID = paste0("ID", 1:10000), A.1 = c(10, rnorm(9999)), A.2 = c(10, rnorm(9999)), B.1 = c(6, rnorm(9999)), B.2 = c(6, rnorm(9999)), C.1 = c(-6, rnorm(9999)), C.2 = c(-6, rnorm(9999)), C.3 = c(-6, rnorm(9999)), stringsAsFactors = FALSE
+)
+datCol <- colnames(dat)[-which(colnames(dat) %in% "ID")]
+myPairs <- unique(sapply(datCol, function(x) unlist(strsplit(x,"[.]"))[1]))
 
 ui <- shinyUI(fluidPage(
   titlePanel("title panel"),
@@ -33,28 +38,38 @@ server <- shinyServer(function(input, output, session) {
   observeEvent(input$goButton, geneNum$x <- geneNum$x + 1)
   observeEvent(input$selPair, geneNum$x <- 0)
   
-  # Create data and subsets of data based on user selection of pairs
-  dat <- data.frame(
-    ID = paste0("ID", 1:10000), A = c(4, rnorm(9999)),
-    B = c(4, rnorm(9999)), C = c(4, rnorm(9999)), D = c(4, rnorm(9999)),
-    stringsAsFactors = FALSE
-  )
-  
   # Create data subset based on two letters user chooses
   datSel <- eventReactive(input$selPair, {
     validate(need(length(input$selPair) == 2, "Select a pair."))
-    dat[c("ID", input$selPair)]
+    sampleIndex <- reactive(which(sapply(colnames(dat), function(x) unlist(strsplit(x,"[.]"))[1]) %in% c(input$selPair[1], input$selPair[2])))
+    dat[,c(1, sampleIndex())]
   }, ignoreNULL = FALSE)
+  
+  output$info <- renderPrint({str(datSel())})
   
   # Create background Plotly graph with hex binning all 100 rows of the two user-selected columns
   ggPS <- eventReactive(datSel(), {
+    
+    sampleIndex1 <- which(sapply(colnames(datSel()), function(x) unlist(strsplit(x,"[.]"))[1]) %in% c(input$selPair[1]))
+    sampleIndex2 <- which(sapply(colnames(datSel()), function(x) unlist(strsplit(x,"[.]"))[1]) %in% c(input$selPair[2]))
+    
     minVal = min(datSel()[,-1])
     maxVal = max(datSel()[,-1])
     maxRange = c(minVal, maxVal)
     xbins=7
-    buffer = (maxRange[2]-maxRange[1])/xbins/2
-    x = unlist(datSel()[input$selPair[1]])
-    y = unlist(datSel()[input$selPair[2]])
+    buffer = (maxRange[2]-maxRange[1])/(xbins/2)
+    #x = unlist(datSel()[,(sampleIndex1)])
+    #y = unlist(datSel()[,(sampleIndex2)])
+    
+    x <- c()
+    y <- c()
+    for (i in 1:length(sampleIndex1)){
+      for (j in 1:length(sampleIndex2)){
+        x <- c(x, unlist(datSel()[,(sampleIndex1[i])]))
+        y <- c(y, unlist(datSel()[,(sampleIndex2[j])]))
+      }
+    }
+    
     h <- hexbin(x=x, y=y, xbins=xbins, shape=1, IDs=TRUE,
                 xbnds=maxRange, ybnds=maxRange)
     hexdf <- data.frame (hcell2xy (h),  hexID = h@cell, counts = h@count)
@@ -70,7 +85,7 @@ server <- shinyServer(function(input, output, session) {
   })
   
   # Output ID of selected row
-  output$info <- renderPrint({ datSel()$ID[req(geneNum$x)] })
+  # output$info <- renderPrint({ datSel()$ID[req(geneNum$x)] })
   
   # Output hex bin plot created just above
   output$scatMatPlot <- renderPlotly({
